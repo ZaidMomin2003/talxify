7948415d289f00fe41b73aa50c5afbbfa6623559
rashidabegum Momin <rashidabegummomin09@gmail.com>
2025-10-18 03:07:45 +0000

    use this code, i added color styking : /* First make sure that you have

--- src/app/earn-money/cal-embed.tsx
+++ src/app/earn-money/cal-embed.tsx
@@ -7,11 +7,11 @@ import { useEffect } from "react";
 export default function CalEmbed() {
   useEffect(() => {
     (async function () {
-      const cal = await getCalApi();
-      cal("ui", {"hideEventTypeDetails":false,"layout":"month_view"});
+      const cal = await getCalApi({"namespace":"30min"});
+      cal("ui", {"cssVarsPerTheme":{"light":{"cal-brand":"#595858"},"dark":{"cal-brand":"#a60808"}},"hideEventTypeDetails":false,"layout":"month_view"});
     })();
   }, [])
-  return <Cal
+  return <Cal namespace="30min"
     calLink="zaid-talxify/30min"
     style={{width:"100%",height:"100%",overflow:"scroll"}}
     config={{"layout":"month_view"}}
